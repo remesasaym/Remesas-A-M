4d29d7892bb8fdadba8f50c25ab08e2f39dcea0c
Remesas Deployer <deploy@remesas-am.com>
2025-12-07 21:13:03 -0500

    feat: Añade el logo de la marca y su componente React para visualización.

--- components/icons/LogoIcon.tsx
+++ components/icons/LogoIcon.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import logoImage from '../../src/assets/logo.png';
 
 const LogoIcon: React.FC<{ className?: string }> = ({ className }) => (
   <img
-    src={logoImage}
+    src="/brand-logo.png"
     alt="Logo de Remesas A&M"
     className={className}
   />
